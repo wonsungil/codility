7098e238947082c6e9998ba60ed75bb8558683bd
wonsungil <sungil.won@tlx.co.kr>
2018-11-07 17:45:28 +0900

    all_counter 시 업데이트 방식 변경

--- src/codility/lesson4_counting_elements/MaxCounter.java
+++ src/codility/lesson4_counting_elements/MaxCounter.java
@@ -3,7 +3,8 @@ package codility.lesson4_counting_elements;
 public class MaxCounter {
 
     /**
-     * 77/100 속도 고려해서 재작성 필요
+     * all_counter 시 최대값을 저장한 뒤 마지막에 N만큼 반복하면서 마지막 최대 보다 작은 counter를 업데이트 해준다.
+     *
      * @param N
      * @param A
      * @return
@@ -11,23 +12,39 @@ public class MaxCounter {
     public static int[] solution(int N, int[] A) {
 
         int[] counter = new int[N];
-        int maxVal = 0;
-        int index = 0;
-        for (int i = 0; i < A.length;  i++) {
+        int lastUpdate = 0;
+        int maxValue = 0;
+
+        for (int i=0 ; i<A.length; i++) {
 
             if(A[i] == N+1) {
-                for (int j=0 ; j < N; j++) {
-                    counter[j] = maxVal;
-                }
 
-            } else {
-                index = A[i] - 1;
-                counter[index]++;
-                if(counter[index] > maxVal) {
-                    maxVal = counter[index];
-                }
+                lastUpdate = maxValue;
+
+            } else if (A[i] <= N){
+               counter[A[i]-1]++;
+
+               if(counter[A[i]-1] <= lastUpdate) {
+                   counter[A[i]-1] = lastUpdate + 1;
+               }
+
+               if(counter[A[i]-1] > maxValue) {
+                   maxValue = counter[A[i]-1];
+               }
+            }
+
+            for(int k=0 ; k<counter.length; k++) {
+                System.out.print(counter[k] + " ");
+            }
+            System.out.println();
+        }
+
+        for (int i=0 ; i<counter.length; i++) {
+            if(counter[i] < lastUpdate) {
+                counter[i] = lastUpdate;
             }
         }
+
         return counter;
     }
 
@@ -35,9 +52,7 @@ public class MaxCounter {
         int[] A = {3, 4, 4, 6, 1,  4, 4};
         int[] B = {2, 1, 1, 2, 1};
 
-        int[] result = solution(5, A);
-
-        int[] result2 = solution(1, B);
+        int[] result2 = solution(5, B);
 
         for (int item: result2) {
             System.out.print(item + " ");
