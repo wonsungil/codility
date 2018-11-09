258a290e145067ffaa4f3c90b00f3211283db58c
wonsungil <sungil.won@tlx.co.kr>
2018-11-09 17:58:40 +0900

    TennisTournament

--- src/codility/lesson92_2016_college_coders/TennisTournament.java
+++ src/codility/lesson92_2016_college_coders/TennisTournament.java
@@ -2,6 +2,12 @@ package codility.lesson92_2016_college_coders;
 
 public class TennisTournament {
 
+    /**
+     * 최대 매치 수, 코트 수 중 최소값
+     * @param P
+     * @param C
+     * @return
+     */
     public static int solution(int P, int C) {
         int maxMatch = P/2;
         return Math.min(maxMatch, C);
