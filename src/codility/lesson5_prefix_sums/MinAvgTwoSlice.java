b47021b51fa1acad4ef7d42c49fa82b6db344ee9
wonsungil <sungil.won@tlx.co.kr>
2018-11-13 14:47:10 +0900

    O(N^3) -> O(N^2)

--- src/codility/lesson5_prefix_sums/MinAvgTwoSlice.java
+++ src/codility/lesson5_prefix_sums/MinAvgTwoSlice.java
@@ -2,39 +2,31 @@ package codility.lesson5_prefix_sums;
 
 public class MinAvgTwoSlice {
 
-    public static int solution(int[] A) {
+    public static int solution2(int[] A) {
+        int minPos = 0;
+        float min = -1;
+        int curSum = 0;
+        float curAvg;
 
-        if(A.length==1) {
-            return A[0];
-        }
-
-        float minAvg = -1;
-        int minStartPos = 0;
-        int sum = 0;
-        int p = 0;
-        int q = 0;
-
-        for (int i=0; i<A.length ; i++) {
-            for(int j=i+1; j<A.length; j++) {
-                for (int k=i; k<=j; k++) {
-                    sum += A[k];
+        for (int i=0; i<A.length; i++) {
+            for (int j=i; j<A.length; j++) {
+                curSum += A[j];
+                if(j-i != 0 ) {
+                    curAvg = (float)curSum/(j-i+1);
+                    if((min==-1)||(min > curAvg)) {
+                        min = curAvg;
+                        minPos = i;
+                    }
                 }
-                float currentAvg = (float)sum/(j+1-i);
-                if((minAvg==-1) || (minAvg>currentAvg)) {
-                    minAvg = currentAvg;
-                    minStartPos = i;
-                }
-
-                sum = 0;
             }
+            curSum = 0;
         }
-
-        return minStartPos;
+        return minPos;
     }
 
     public static void main(String[] args) {
         int[] A = {4, 2, 2, 5, 1, 5, 8};
 
-        System.out.println(solution(A));
+        System.out.println(solution2(A));
     }
 }
