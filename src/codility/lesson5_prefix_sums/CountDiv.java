2587b6d00876f9cf4b2dbb31efab7aed73c00b34
wonsungil <sungil.won@tlx.co.kr>
2018-11-13 18:41:44 +0900

    count div

---
+++ src/codility/lesson5_prefix_sums/CountDiv.java
@@ -0,0 +1,27 @@
+package codility.lesson5_prefix_sums;
+
+public class CountDiv {
+
+    public static int solution(int A, int B, int K) {
+        int count = 0;
+        for (int i=A; i<=B; i++) {
+            if(i % K == 0) {
+                System.out.println(i);
+                count++;
+            }
+        }
+        return count;
+    }
+
+    public static void main(String[] args) {
+        int A = 2;
+        int B = 100;
+        int K = 2;
+
+        long startTime = System.currentTimeMillis();
+        System.out.println(solution(A, B, K));
+        long endTime = System.currentTimeMillis();
+        System.out.println(endTime - startTime);
+
+    }
+}
--- src/codility/lesson5_prefix_sums/MinAvgTwoSlice.java
+++ src/codility/lesson5_prefix_sums/MinAvgTwoSlice.java
@@ -8,6 +8,22 @@ public class MinAvgTwoSlice {
         int curSum = 0;
         float curAvg;
 
+        int totalSum = 0;
+        float totalAvg = 0;
+
+        for (int i=0; i<A.length ; i++) {
+            totalSum += A[i];
+        }
+        totalAvg = (float)totalSum / (A.length+1);
+        System.out.println(totalAvg);
+
+        for (int i=0; i<A.length; i++) {
+            totalSum -= A[i];
+            System.out.println(totalSum);
+            System.out.println((float)totalSum/A.length-(i+1));
+        }
+
+
         for (int i=0; i<A.length; i++) {
             for (int j=i; j<A.length; j++) {
                 curSum += A[j];
