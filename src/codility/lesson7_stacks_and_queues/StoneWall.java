beba679fc1fbdd451e4ed901eb559f01825ed029
wonsungil <sungil.won@tlx.co.kr>
2018-11-22 14:05:28 +0900

    StoneWall 100/100

--- src/codility/lesson7_stacks_and_queues/StoneWall.java
+++ src/codility/lesson7_stacks_and_queues/StoneWall.java
@@ -1,40 +1,44 @@
 package codility.lesson7_stacks_and_queues;
 
-import java.sql.Statement;
 import java.util.Stack;
 
+/**
+ * 100/100
+ * code 정리필요.
+ *  1. H Loop
+ *  2. 스택이 비었다면 push
+ *  3. 빈 스택이 아니면
+ *      3-1. peek 과 i가 같다면 push(i)
+ *      3-2. peek이 i보다 작다면 push(i)
+ *      3-3. peek이 i보다 크다면 stack에서 peek과 같은 값들 모두 Pop, numOfBlock++
+ *  4. 빈 스택이 아니라면, peek후 같은 값들 pop, numOfBlock++
+ */
 public class StoneWall {
 
-    public void print(Stack<Integer> S) {
-        Stack<Integer> temp = (Stack<Integer>)S.clone();
-        while (!temp.isEmpty()) {
-            System.out.print( temp.pop() + ", ");
+    public void popStone(Stack<Integer> S, int value) {
+        while(!S.isEmpty() && S.peek() == value) {
+            S.pop();
         }
-
     }
 
-    public int solution2(int[] H) {
+    public int solution(int[] H) {
 
         int numOfBlock = 0;
 
         Stack<Integer> stack = new Stack<>();
 
         for (int i: H) {
-            print(stack);
-            System.out.println("[" + numOfBlock + "]");
-
             if(stack.isEmpty()) {
                 stack.push(i);
             } else {
                 boolean loop = true;
                 while (loop) {
                     int peek = stack.peek();
-
                     if(peek == i) {
-                        stack.pop();
-                        numOfBlock++;
+                        stack.push(i);
+                        loop = false;
                     } else if (peek > i) {
-                        stack.pop();
+                        popStone(stack, peek);
                         numOfBlock++;
 
                         if(!stack.isEmpty()) {
@@ -44,6 +48,7 @@ public class StoneWall {
                             }
                         } else {
                             stack.push(i);
+                            loop = false;
                         }
                     } else if(peek < i) {
                         stack.push(i);
@@ -58,37 +63,9 @@ public class StoneWall {
             }
         }
 
-        if(!stack.isEmpty()) {
-            numOfBlock+=stack.size();
-        }
-
-        return numOfBlock;
-    }
-
-    public int solution(int[] H) {
-        Stack<Integer> wallStack = new Stack<>();
-
-        int numOfBlock = 0;
-        for (int i : H) {
-            if(wallStack.isEmpty()) {
-                wallStack.push(i);
-            } else {
-                int peek = wallStack.peek();
-                if(peek == i) {
-                    wallStack.pop();
-                    numOfBlock++;
-                } else if(peek > i) {
-                    wallStack.pop();
-                    numOfBlock++;
-                    wallStack.push(i);
-                } else if(peek < i) {
-                    wallStack.push(i);
-                }
-            }
-        }
-
-        if(wallStack.size() > 0) {
-            numOfBlock+=wallStack.size();
+        while(!stack.isEmpty()) {
+            popStone(stack, stack.peek());
+            numOfBlock++;
         }
 
         return numOfBlock;
@@ -98,8 +75,13 @@ public class StoneWall {
 
         int[] H = {8, 8, 5, 7, 9, 8, 7, 4, 8};
         int[] H2 = {3,2,1};
+        int[] H3 = {1,1,1};
+        int[] H4 = {1,2,1};
 
         StoneWall stoneWall = new StoneWall();
-        System.out.println(stoneWall.solution2(H));
+        System.out.println(stoneWall.solution(H));
+        System.out.println(stoneWall.solution(H2));
+        System.out.println(stoneWall.solution(H3));
+//        System.out.println(stoneWall.solution(H4));
     }
 }
